8662ae0fc2c9dc9c1f74a74719abfe9fcd45059e
xumingming64398966 <xumingming64398966@415408d5-95df-7dfe-14a4-35b1c11b659b>
2010-08-20 12:50:54 +0000

    separate the css into a seprate /style.css url.

--- pastebin-server.py
+++ pastebin-server.py
@@ -25,40 +25,12 @@ import cgi
 HTTP_PORT = 80
 
 FORM = """
-<html>
+<html class="html">
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
         <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Cantarell">
-        <style>
-        body {
-              font-family: 'Cantarell', serif;
-              font-size: 48px;
-              text-shadow: 4px 4px 4px #aaa;
-        }
-        textarea {
-              font-family: 'Cantarell', serif;
-              font-size: 16px;
-        }
-        a {
-              text-decoration: none;
-        }
-
-        .content {
-            width: 80%;
-            height: 80%;
-            margin-left: 10%;
-            margin-top: 1%;
-            display: block;
-            background-color:#eef;
-            border: solid 1px dotted;
-        }
-        .button {
-            display:block;
-            width:100px;
-            height:50px;
-            margin-left:45%;
-        }
-        </style>
+        <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Reenie+Beanie">
+        <link rel="stylesheet" type="text/css" href="%(CONTEXT_PATH)s/style.css">
     </head>
     <body style="font-size: 50">
         <form action="/pasteit" method="POST">
@@ -75,22 +47,114 @@ CONTENT_TEMPLATE = """
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
         <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Droid+Sans+Mono">
         <link rel="stylesheet" type="text/css" href="http://shjs.sourceforge.net/sh_style.css">
+        <link rel="stylesheet" type="text/css" href="%(CONTEXT_PATH)s/style.css">
+
         <script src="http://shjs.sourceforge.net/sh_main.min.js" type="text/javascript"></script>
         <script type="text/javascript" src="http://shjs.sourceforge.net/lang/sh_%(LANG)s.js"></script>
-        <style>
-        body {
-              font-family: 'Droid Sans Mono', serif;
-              font-size: 15px;
-        }
-        a {
-              text-decoration: none;
-        }
-        </style>
     </head>
     <body onload="sh_highlightDocument();">
+        <div id="ul_parent">
+            <ul id="lang_list_%(LANG)s">
+                <li><a class="lang_java" href="%(CONTEXT_PATH)s/java/%(PASTEBIN_FILE_NAME)s">java</a></li>
+                <li><a class="lang_python" href="%(CONTEXT_PATH)s/python/%(PASTEBIN_FILE_NAME)s">python</a></li>
+                <li><a class="lang_javascript" class="" href="%(CONTEXT_PATH)s/javascript/%(PASTEBIN_FILE_NAME)s">javascript</a></li>
+                <li><a class="lang_css" href="%(CONTEXT_PATH)s/css/%(PASTEBIN_FILE_NAME)s">css</a></li>
+                <li><a class="lang_html" href="%(CONTEXT_PATH)s/html/%(PASTEBIN_FILE_NAME)s">html</a></li>
+                <li><a class="lang_cpp" href="%(CONTEXT_PATH)s/cpp/%(PASTEBIN_FILE_NAME)s">cpp</a></li>
+                <li><a class="plain" href="%(CONTEXT_PATH)s/plain/%(PASTEBIN_FILE_NAME)s">plain</a></li>
+            </ul>
+        </div>
 	    <pre class="sh_%(LANG)s">%(CONTENT)s</pre>
     </body>
 </html>
+"""
+
+STYLE_CSS = """
+.html {
+    background: url(http://emotionslive.co.uk/img/shadow.jpg) repeat;
+}
+body {
+  font-family: 'Cantarell', serif;
+  font-size: 48px;
+  text-shadow: 4px 4px 4px #aaa;
+}
+textarea {
+      font-family: 'Cantarell', serif;
+      font-size: 16px;
+}
+a {
+   text-decoration: none;
+}
+a:hover {
+}
+ul {
+    display: block;
+    font-family: 'Cantarell', serif;
+    font-size: 20px;
+    text-shadow: none;
+    border: 1px solid dotted;
+    height: 100px;
+    width: 80%;
+    margin-top: 2%;
+    margin-left: 2%;
+    position: relative;
+}
+li {
+    display: inline;
+    margin-left: 5px;
+}
+
+pre {
+    font-family: 'Cantarell', serif;
+    font-size: 16px;
+    text-shadow: none;
+    background-color: white;
+    margin-left: 5%;
+}
+.content {
+    width: 80%;
+    height: 80%;
+    margin-left: 10%;
+    margin-top: 1%;
+    display: block;
+    background-color:#eef;
+    border: solid 1px dotted;
+}
+
+.button {
+    display:block;
+    width:100px;
+    height:50px;
+    margin-left:45%;
+    font-family: 'Reenie Beanie';
+    font-weight: bold;
+}
+
+.ul_parent {
+    position: absolute;
+}
+
+#lang_list_java .lang_java {
+    font-weight: bold;
+}
+#lang_list_python .lang_python {
+    font-weight: bold;
+}
+#lang_list_javascript .lang_javascript {
+    font-weight: bold;
+}
+#lang_list_html .lang_html {
+    font-weight: bold;
+}
+#lang_list_css .lang_css {
+    font-weight: bold;
+}
+#lang_list_cpp .lang_cpp {
+    font-weight: bold;
+}
+#lang_list_plain .lang_plain {
+    font-weight: bold;
+}
 """
 
 DATA_FOLDER_NAME = "data"
@@ -101,35 +165,54 @@ class MyHandler(SimpleHTTPRequestHandler):
     def do_GET(self):
         self.init_params()
 
-        if self.path not in ["/favicon.ico", URL_DATA_FOLDER]:
+        if self.path not in ["/favicon.ico", "/style.css", URL_DATA_FOLDER]:
             self.log_request()
             self.send_response(200)
             self.send_header("Content-Type", "text/html;charset=UTF-8")
             self.end_headers()
 
             if self.path == "/":
-                self.wfile.write(FORM)
+                self.wfile.write(FORM % {"CONTEXT_PATH": self.get_context_path()})
 
             # get the clean content: no html
             elif self.path.find("/plain/") == 0:
                 filename = DATA_FOLDER_PATH + self.path[6:]
                 content = self.read_file(filename)
                 self.wfile.write(cgi.escape(content))
+
+            # language(java, css) is specified
             elif len(self.path.split("/")) > 2:
                 splits = self.path.split("/")
                 lang = splits[1]
+                self.pastebin_file_name = splits[2]
                 filename = DATA_FOLDER_PATH + self.path[len(lang) + 1:]
                 content = self.read_file(filename)
-                self.wfile.write(CONTENT_TEMPLATE % {"LANG": lang, "CONTENT":cgi.escape(content)})
+                self.wfile.write(CONTENT_TEMPLATE % {"LANG": lang,
+                    "CONTENT":cgi.escape(content),
+                    "CONTEXT_PATH": self.get_context_path(),
+                    "PASTEBIN_FILE_NAME": self.pastebin_file_name})
+
+            # no language is specified
             else:
+                self.pastebin_file_name = self.path[1:]
                 filename = DATA_FOLDER_PATH + self.path
                 if self.path.find(URL_DATA_FOLDER) == 0:
                     filename = "." + self.path
 
                 content = self.read_file(filename)
-                self.wfile.write(CONTENT_TEMPLATE % {"LANG": "java", "CONTENT":cgi.escape(content)})
+                self.wfile.write(CONTENT_TEMPLATE % {"LANG": "java",
+                    "CONTENT":cgi.escape(content),
+                    "CONTEXT_PATH": self.get_context_path(),
+                    "PASTEBIN_FILE_NAME": self.pastebin_file_name})
         else:
-            return SimpleHTTPRequestHandler.do_GET(self)
+            if self.path == "/style.css":
+                self.log_request()
+                self.send_response(200)
+                self.send_header("Content-Type", "text/css;charset=UTF-8")
+                self.end_headers()
+                self.wfile.write(STYLE_CSS)
+            else:
+                return SimpleHTTPRequestHandler.do_GET(self)
 
     def do_POST(self):
         self.init_params()
@@ -151,6 +234,9 @@ class MyHandler(SimpleHTTPRequestHandler):
         f.close()
 
         return content
+
+    def get_context_path(self):
+        return "http://" + self.headers['host']
 
     def init_params(self):
         """Get the params from url and request body
